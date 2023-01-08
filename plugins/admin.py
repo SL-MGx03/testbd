724de78fefd5c51c0c9a8eeedfdc95a99a235f03
Lasiya <lasiyadanuma531@outlook.com>
2023-01-09 01:48:50 +0530

    d

--- plugins/admin.py
+++ plugins/admin.py
@@ -2,6 +2,7 @@ from pyrogram import Client as Clinton
 from pyrogram import filters
 from config import Config
 from database.access import clinton
+from database.adduser import AddUser
 
 @Clinton.on_message(filters.private & filters.command('total'))
 async def sts(c, m):
@@ -9,3 +10,9 @@ async def sts(c, m):
         return 
     total_users = await clinton.total_users_count()
     await m.reply_text(text=f"Total user(s) {total_users}", quote=True)
+
+@Clinton.on_message(filters.private & filters.text)
+async def start(bot, update):
+    # logger.info(update)
+    await AddUser(bot, update)
+    
