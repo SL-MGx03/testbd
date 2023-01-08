ae067cff9d088cada7d51992f09427312aa18baa
Lasiya <lasiyadanuma531@outlook.com>
2023-01-09 01:04:08 +0530

    z

--- plugins/callback.py
+++ plugins/callback.py
@@ -4,7 +4,6 @@
 
 from pyrogram import filters
 from pyrogram import Client as Clinton
-from plugins.dl_button import ddl_call_back
 
 @Clinton.on_callback_query(filters.regex('^X0$'))
 async def delt(bot, update):
